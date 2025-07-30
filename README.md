f05f00a03cdcb73dc7ebdeb429ae3a9940e2efc1
LGYS <18207468101@qq.com>
2025-07-30 09:42:53 +0800

    第一次发布

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# golgys
+### goWatcher
+goWatcher - go项目文件监听器，自动编译运行。
+
+### 安装命令
+```cmd
+go install github.com/LGYS07/golgys@latest
+```
+
+### macOS 将goWatcher 复制到 /usr/local/bin
+```cmd
+~/go/bin/golgys install
+```
+
+### 使用
+```cmd
+golgys
+```
+
+### 查看版本
+```cmd
+golgys v
+```
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/LGYS07/golgys/tools"
+	"github.com/fsnotify/fsnotify"
+)
+
+var (
+	// 版本号
+	Version = "v1.0.0"
+)
+
+func main() {
+
+	// 命令参数
+	if len(os.Args) > 1 {
+		if os.Args[1] == "install" {
+			tools.Install()
+			return
+		} else if os.Args[1] == "v" || os.Args[1] == "version" {
+			println("golgys " + Version)
+			return
+		}
+	}
+
+	// 文件监听
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal("✘ 创建监听器失败:", err)
+	}
+	defer watcher.Close()
+
+	// 上一个事件
+	var lastEvent fsnotify.Event = fsnotify.Event{}
+
+	// 启动监听协程
+	go func() {
+		time.Sleep(time.Second * 5)
+		for {
+			select {
+			// 有文件变化
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Rename == fsnotify.Rename {
+					watcher.Add(event.Name)
+				}
+				// 新建目录
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					// 添加监听
+					if info, err := os.Stat(event.Name); err == nil && info.IsDir() {
+						watcher.Add(event.Name)
+						log.Printf("› 新增目录监听: %s\n", event.Name)
+					}
+				}
+				// 只处理 .go 文件的变化
+				if !strings.HasSuffix(event.Name, ".go") {
+					continue
+				}
+				// 文件保存事件
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					lastEvent = event
+					log.Printf("› 检测到文件变化: %s\n", event.Name)
+				}
+			// 没有变化
+			default:
+				time.Sleep(time.Second * 2)
+				if lastEvent.Name != "" {
+					// 终止之前的进程
+					tools.KillProcess()
+					// 重新编译并运行
+					tools.BuildAndRun()
+					// 重置上一次事件
+					lastEvent.Name = ""
+					time.Sleep(time.Second * 2)
+				}
+			}
+
+		}
+	}()
+
+	// 获取当前目录
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 添加当前目录监听
+	watcher.Add(dir)
+	// 监听子目录
+	// 递归监听当前目录下的所有子目录
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			// 排除一些不需要监听的目录
+			if tools.ShouldIgnoreDir(path) {
+				return filepath.SkipDir
+			}
+			return watcher.Add(path)
+		}
+		return nil
+	})
+
+	log.Println("› 开始监听文件变化 ...")
+
+	// 首次运行
+	tools.BuildAndRun()
+	for {
+		time.Sleep(time.Second * 10)
+	}
+}
